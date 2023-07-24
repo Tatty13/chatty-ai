df0e9cc40231957df989270238ed1487a6057efb
RinatMujaurov <ifloki@yandex.ru>
2023-07-24 14:49:59 +0300

    feat: add basic page layout

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chat from '../Chat/Chat'; 
+
+const Main = () => {
+  return (
+    <main className='content'>
+      <Chat /> 
+    </main>
+  );
+};
+
+export default Main;
