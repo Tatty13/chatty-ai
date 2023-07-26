a028093b2344d99047279a792421e40fc52fde81
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-07-26 23:01:50 +0300

    refactor: add files with initial messages and languages data

---
+++ src/components/Chat/initialMessages.js
@@ -0,0 +1,23 @@
+const initialMessages = [
+  {
+    sender: 'bot',
+    content:
+      "Hey there, nice to meet you! I’m Chatty your personal AI interview coach who will help you prepare for interviews and overcome your fear of tricky questions for the position of UX/UI Designer. With me, you'll stop being afraid of interviews and be one step closer to landing your dream job!",
+  },
+  {
+    sender: 'bot',
+    content: 'So, for how many days do you want to prepare for the interview?',
+  },
+
+  {
+    sender: 'user',
+    content: 'User message here',
+  },
+
+  {
+    sender: 'poll',
+    content: 'User choice',
+  },
+];
+
+export default initialMessages;
