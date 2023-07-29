45b60e53ce005f031b107c2836e9df47402cdaf6
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-07-29 11:35:39 +0300

    feat: add hover effect and star icon for messages

--- src/components/MessageList/MessageList.jsx
+++ src/components/MessageList/MessageList.jsx
@@ -2,12 +2,18 @@ import React, { forwardRef } from 'react';
 
 import './MessageList.css';
 
-const MessageList = ({ messages }, ref) => {
+const MessageList = ({ messages, isSaved }, ref) => {
   const messageItems = messages?.map((message, i) => (
     <li
       key={i}
-      className={`chat__message chat__message__sender_${message.sender}`}>
+      className={`chat__message chat__message_sender_${message.sender}`}>
       <p className='chat__message_text'>{message.content}</p>
+      <button
+        type='button'
+        className={`btn chat__message-btn chat__message-btn_sender_${
+          message.sender
+        } ${isSaved ? 'chat__message-btn_saved' : ''}`}
+      />
     </li>
   ));
   return (
